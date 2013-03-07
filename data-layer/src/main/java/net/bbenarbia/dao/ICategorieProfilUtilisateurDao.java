c5977ca2683bab7a12fdc9d46afa71b9f6054b66
moad <ben.benarbia@gmail.com>
2013-03-07 22:51:48 +0100

    adding generic service

--- data-layer/src/main/java/net/bbenarbia/dao/ICategorieProfilUtilisateurDao.java
+++
@@ -1,8 +0,0 @@
-package net.bbenarbia.dao;
-
-import net.bbenarbia.dao.common.IGenericDao;
-import net.bbenarbia.domain.CategorieProfilUtilisateur;
-
-public interface ICategorieProfilUtilisateurDao extends IGenericDao<CategorieProfilUtilisateur> {
-
-}
