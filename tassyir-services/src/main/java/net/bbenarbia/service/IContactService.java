a32109bb09a722a4fa025a3c091770a6709bba34
bbenarbia <bbenarbia@FRU00958.softeam.com>
2013-04-15 15:50:23 +0200

    delete Contact entity

--- tassyir-services/src/main/java/net/bbenarbia/service/IContactService.java
+++
@@ -1,8 +0,0 @@
-package net.bbenarbia.service;
-
-import net.bbenarbia.domain.Contact;
-import net.bbenarbia.service.generic.IGenericService;
-
-public interface IContactService extends IGenericService<Contact>{
-
-}
